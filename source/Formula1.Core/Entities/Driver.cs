bde78d158d64714162e87a7697a217a492714c3b
Rene Lochner <school@renelochner.at>
2020-03-04 21:56:21 +0100

    Implement "Driver" class

--- source/Formula1.Core/Entities/Driver.cs
+++ source/Formula1.Core/Entities/Driver.cs
@@ -1,10 +1,24 @@
 ﻿using Formula1.Core.Contracts;
+using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace Formula1.Core.Entities
 {
     public class Driver : ICompetitor
     {
-        public string Name { get; }
+        // public int Number { get; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //public DateTime Birthday { get; set; }
+        public string Nationality { get; set; }
+
+        public string Name => ToString();
+
+        public override string ToString()
+        {
+            return $"{LastName} {FirstName}";
+        }
     }
 }
